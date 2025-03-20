cd5e1d223987ef80cc4435b9a02a2008509b525f
Aarushi <aarushimandloi744@gmail.com>
2025-03-21 02:29:17 +0530

    sorted

---
+++ Sorte.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main(){
+    
+    int a;
+    scanf("%d",&a);
+int array[a];
+int size = sizeof array / sizeof array[0];
+  for(int i=0 ; i<size ; i++){
+    scanf("%d",&array[i]);
+  }
+int isAscending = 1;
+int isDecending = 1;
+ int i;
+for(i=1;i<size-1;i++){
+    if(array[i] > array[i+1]){  
+        isAscending = 0;
+    }
+    else if(array[i] < array[i+1]){
+        isDecending = 0;
+    }
+}
+if(isAscending) { 
+    printf("sorted in isAscending");
+}
+else if(isDecending){
+    printf("sorted in isDecending");
+}
+else{
+    printf("UNsorted");
+}
+  return 0;
+}
