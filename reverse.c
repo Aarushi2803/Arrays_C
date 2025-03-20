b865d57422c64becfe952eeb966ae9419bab233f
Aarushi <aarushimandloi744@gmail.com>
2025-03-21 03:26:46 +0530

    reverse

---
+++ reverse.c
@@ -0,0 +1,24 @@
+//reversing an array
+#include <stdio.h>
+int main(){
+    
+    int a;
+    scanf("%d",&a);
+int array[a];
+int size = sizeof array / sizeof array[0];
+  for(int i=0 ; i<size ; i++){
+    scanf("%d",&array[i]);
+  }
+  
+ int b[size];
+ for(int i=0;i<=size/2;i++){
+    int temp = array[i];
+    array[i] =  array[size-i-1];
+    array[size-i-1] = temp; 
+    
+ } 
+ for(int i=0;i<size;i++){
+    printf("%d ",array[i]); 
+ } 
+  return 0;
+}
