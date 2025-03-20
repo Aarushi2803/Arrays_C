db10e5b854a23a2f24ef0e5f4a4ae7effe1f88d2
Aarushi <aarushimandloi744@gmail.com>
2025-03-21 04:39:27 +0530

    sorting

---
+++ sortingarray.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main(){
+    
+int a;
+scanf("%d",&a);
+int array[a];
+int size = sizeof array / sizeof array[0];
+  for(int i=0 ; i<size ; i++){
+    scanf("%d",&array[i]);
+  }
+for(int i=0;i<size;i++){
+    int flag = 1;
+    for(int j=0;j<size-i-1;j++){
+        if(array[j] > array[j+1]){
+            int temp =array[j];
+            array[j]=array[j+1];
+            array[j+1]=temp;
+        flag = 0;
+        }
+    }
+    if(flag) break;
+}
+
+for(int i=0;i<size;i++){
+    printf("%d ",array[i]);
+}
+ 
+  return 0;
+}
+
+
