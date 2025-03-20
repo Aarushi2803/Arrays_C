c5d5701ab16d091981647652493f8d2f614b82db
Aarushi <aarushimandloi744@gmail.com>
2025-03-21 03:43:19 +0530

    key

---
+++ FindKeyElement.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+    
+    int a;
+    scanf("%d",&a);
+int array[a];
+int size = sizeof array / sizeof array[0];
+  for(int i=0 ; i<size ; i++){
+    scanf("%d",&array[i]);
+  }
+  int key,index,i; 
+  scanf("%d",&key);
+  
+ for( i=0;i<size;i++){
+    if(array[i] == key){
+        index = i; 
+     break;
+    } 
+ }
+ if(i==size) printf("not found");
+ else printf("found at index %d",index);
+  return 0;
+}
